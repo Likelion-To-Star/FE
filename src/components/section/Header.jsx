4968122a9b881115bcb1b8f0123c55ca09317b19
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2024-10-16 22:50:40 +0900

    폴더 및 header ongoing

---
+++ src/components/section/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../../assets/img/upper-logo.svg'
+import Mypage from '../../assets/img/mypage.svg'
+
+const Header = () => {
+  return (
+    <div className='header-wrap'>
+        <div className='hd-title'>
+            <img className="logo"src={Logo} alt="logo" />
+            <button><img src={Mypage} alt="Tomypage" /></button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
