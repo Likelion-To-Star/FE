f3e7a941727fda9d3a815f725c5e503bdc6b2b60
oculo0204 <96184457+oculo0204@users.noreply.github.com>
2024-10-23 21:30:30 +0900

    [chore] 서브라우터 메인 하위로 연결

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./components/main/Main.jsx";
 import Friends from "./components/main/friends/Friends.jsx"; 
-import Community from "./components/main/community/Community.jsx"; // 커뮤니티 관련 컴포넌트
-import Stars from "./components/main/toStar/Stars.jsx"; // 별이에게 관련 컴포넌트
+import Community from "./components/main/community/Community.jsx"; 
+import Stars from "./components/main/toStar/Stars.jsx"; 
 import './assets/scss/styles.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/friends' element={<Friends />} />
-        <Route path='/community' element={<Community />} />
-        <Route path='/stars' element={<Stars />} />
+        {/* 메인 경로 */}
+        <Route path='/main/*' element={<Main />} >
+          {/* Main 내부 하위 라우팅 */}
+          <Route path='community' element={<Community />} />
+          <Route path='friends' element={<Friends />} />
+          <Route path='stars' element={<Stars />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Header from './section/Header'
-import Nav from './section/Nav'
+import React from 'react';
+import Header from './section/Header';
+import Nav from './section/Nav';
+import { Outlet } from 'react-router-dom';
 
 const Main = () => {
   return (
     <div className='main-wrap'>
-        <Header/>
-        <Nav/>
+        <Header />
+        <Nav />
+        <Outlet />
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
+
